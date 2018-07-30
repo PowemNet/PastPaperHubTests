9303b8c4e326259e1f0bb7df59e8e3b8245c7773
Power <powermukisa@gmail.com>
2018-07-30 23:31:30 +0300

    Remove unused code

--- src/test/java/page/dashboard/DashboardStrings.java
+++ src/test/java/page/dashboard/DashboardStrings.java
@@ -2,8 +2,6 @@ package page.dashboard;
 
 public class DashboardStrings {
 
-    public static final String USERNAME = "power";
-    public static final String PASSWORD = "kreso123!";
     public static final String DASHBOARD_TITLE = "Dashboard";
     public static final int NUMBER_OF_ACTVITY_ITEMS_IN_ACTIVITY_STREAM = 10;
 }
